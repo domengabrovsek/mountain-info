9a9ec4fa98c818870c60d88ec348e0dd31d16e48
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2019-11-15 23:01:23 +0100

    Removed some leftover comments

--- index.js
+++ index.js
@@ -14,17 +14,3 @@ app.use(router);
 
 // start server
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
-// const fs = require('fs');
-
-// const {getData} = require('./src/functions');
-
-// getData();
-// let data = {};
-// let folder = "\\files";
-
-// (async function(){
-//   var fileContents = await fs.promises.readFile(FILENAME)
-//   var data = JSON.parse(fileContents)
-// })()
