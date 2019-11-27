44d371b61ebba0e4282f17d21ab921fd8588f693
Kuglll <mark.frelih@gmail.com>
2019-11-27 17:09:54 +0100

    [Mobile] implemented starting activity

--- mobile-app/app/src/main/java/com/example/mountaininfo/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/mountaininfo/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.mountaininfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    static Intent returnMainActiviyIntent(Context ctx) {
+        return new Intent(ctx, MainActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
