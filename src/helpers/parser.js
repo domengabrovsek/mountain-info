37d9a333905d7dc09adadded0ca87f5273793da1
mkloar <miha.kloar@gmail.com>
2019-12-09 14:56:05 +0100

    [API] save mountain routes to DB

--- src/helpers/parser.js
+++ src/helpers/parser.js
@@ -29,10 +29,37 @@ const parse = ($, id) => {
 
 };
 
+
+const parseRoutes = ($, id) => {
+    // parse relevant mountain data from page
+    const mountainRoutesData = $('#vsebina td').text().trim().split('\n').filter(x => x).slice(0, 10).map(x => x.trim());
+
+
+    // parse start coordinates
+    const coordinates = mountainRoutesData[3].split(':')[1].trim().split('/').map(x => x.replace(/°[N|E]{1}/, '').trim());
+
+
+    const moutainRoutes = {
+        id: id,
+        name: mountainRoutesData[0],
+        startName: split(mountainRoutesData[2]),
+        endName: split(mountainRoutesData[4]),
+        startCoordinates: {
+            N: coordinates[0],
+            E: coordinates[1]
+        },
+        time: split(mountainRoutesData[6]),
+        difficultLevel: split(mountainRoutesData[7]),
+        altitudeDifference: split(mountainRoutesData[9])
+    };
+    return moutainRoutes;
+}
+
 // helpers
 
 const split = (input) => input && input.split(': ')[1];
 
 module.exports = {
-    parse
+    parse,
+    parseRoutes
 }
