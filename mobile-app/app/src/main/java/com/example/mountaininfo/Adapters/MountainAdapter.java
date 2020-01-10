fcf705df60adecf852c150c4ee1f6edd4a7cb724
Kuglll <mark.frelih@gmail.com>
2020-01-10 22:10:34 +0100

    [Mobile] random image selection

--- mobile-app/app/src/main/java/com/example/mountaininfo/Adapters/MountainAdapter.java
+++ mobile-app/app/src/main/java/com/example/mountaininfo/Adapters/MountainAdapter.java
@@ -1,22 +1,20 @@
 package com.example.mountaininfo.Adapters;
 
-
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.mountaininfo.API.APIResults;
 import com.example.mountaininfo.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class MountainAdapter extends RecyclerView.Adapter<MountainAdapter.ViewHolder> {
 
@@ -71,10 +69,16 @@ public class MountainAdapter extends RecyclerView.Adapter<MountainAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         APIResults.Mountain currentMountain = mountains.get(position);
-        holder.image.setImageResource(R.drawable.mountain1);
+        holder.image.setImageResource(chooseRandomPicture());
         holder.name.setText(currentMountain.getName());
         holder.range.setText(currentMountain.getRange());
         holder.altitude.setText(Integer.toString(currentMountain.getAltitude()));
+    }
+
+    public int chooseRandomPicture(){
+        List<Integer> images = Arrays.asList(R.drawable.mountain1, R.drawable.mountain2, R.drawable.mountain3, R.drawable.mountain4);
+        Random rnd = new Random();
+        return images.get(rnd.nextInt(images.size()));
     }
 
     @Override
--- mobile-app/app/src/main/java/com/example/mountaininfo/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/mountaininfo/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.mountaininfo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import com.example.mountaininfo.API.DataViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,15 +14,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     FloatingActionButton fab;
-    DataViewModel viewModel = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        viewModel = ViewModelProviders.of(this).get(DataViewModel.class);
-        viewModel.loadRoutes();
 
         initViews();
         initOnClickListeners();
